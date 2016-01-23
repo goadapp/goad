15e78d66fa29758daf09aed7e9219c9d1a4042f7
Joao Carlos <mail@joao-carlos.com>
2016-01-23 22:16:01 +0200

    Invoke lambda

--- goad.go
+++ goad.go
@@ -1,10 +1,14 @@
 package goad
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/gophergala2016/goad/infrastructure"
 	"github.com/gophergala2016/goad/sqsadaptor"
 )
@@ -17,6 +21,10 @@ type TestConfig struct {
 	TotalRequests  uint
 	RequestTimeout time.Duration
 	Region         string
+}
+
+func (c *TestConfig) cmd(sqsURL string) string {
+	return fmt.Sprintf("./goad-lambda %s %d %d %s %s", c.URL, c.Concurrency, c.TotalRequests, sqsURL, c.Region)
 }
 
 type Test struct {
@@ -35,9 +43,23 @@ func (t *Test) Start() <-chan Result {
 	}
 	defer infra.Clean()
 
+	t.invokeLambda(infra.QueueURL())
+
 	results := make(chan sqsadaptor.RegionsAggData)
 	sqsadaptor.Aggregate(results, infra.QueueURL(), t.config.TotalRequests)
 
-	// TODO: add result channel
+	for result := range results {
+		fmt.Println(result)
+	}
 	return nil
+}
+
+func (t *Test) invokeLambda(sqsURL string) {
+	svc := lambda.New(session.New())
+
+	resp, err := svc.InvokeAsync(&lambda.InvokeAsyncInput{
+		FunctionName: aws.String("goad"),
+		InvokeArgs:   strings.NewReader(`{"cmd":"` + t.config.cmd(sqsURL) + `"}`),
+	})
+	fmt.Println(resp, err)
 }
