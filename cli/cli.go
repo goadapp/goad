c870ec3435877a64f52d7e3a12aa6609d7a12e35
Joao Carlos <mail@joao-carlos.com>
2016-01-23 23:07:23 +0200

    Add test CLI

--- cli/cli.go
+++ cli/cli.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gophergala2016/goad"
+import (
+	"time"
+
+	"github.com/gophergala2016/goad"
+)
 
 func main() {
-	test := goad.NewTest(&goad.TestConfig{})
+	test := goad.NewTest(&goad.TestConfig{
+		URL:            "http://dll.nu/",
+		Concurrency:    50,
+		TotalRequests:  1000,
+		RequestTimeout: time.Second,
+		Region:         "eu-west-1",
+	})
 	test.Start()
 }
