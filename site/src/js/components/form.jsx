a06440501f7da64a7cebc9432b7bbe2f13e73164
Matias Korhonen <matias@kiskolabs.com>
2016-01-23 18:28:13 +0200

    Slight tweaks

--- site/src/js/components/form.jsx
+++ site/src/js/components/form.jsx
@@ -9,7 +9,7 @@ export default class Form extends React.Component {
           <input type="url" className="form-control" id="url" placeholder="https://…" />
         </div>
         <div className="form-group">
-          <label>Load</label>
+          <label>Target load</label>
           <p className="form-control-static">10 / second (download the CLI tool to use a higher load)</p>
         </div>
         <button type="submit" className="btn btn-danger">Start test</button>
