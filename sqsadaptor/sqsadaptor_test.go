baa50700bfec555fa4c912b4db533b15076be8ce
Rasmus Sten <rasmus@dll.nu>
2016-01-23 18:18:12 +0200

    Update sqsadaptor test to match implementation

--- sqsadaptor/sqsadaptor_test.go
+++ sqsadaptor/sqsadaptor_test.go
@@ -20,16 +20,19 @@ func TestJSON(t *testing.T) {
 		"2016-01-01 10:00:00",
 		"example.com",
 		"Fetch",
-		"928429348",
 		200,
-		238947,
-		2398,
-		"Finished",
+		int64(12345), // elapsed, first byte
+		int64(6789),  // elapsed, last byte
+		int64(4567),  // elapsed, total
+		4711,         //bytes
+		"Success",
+		"aws-lambda-4711", // AWS lamba instance
+
 	}
 	str, jsonerr := jsonFromResult(result)
 	if jsonerr != nil {
 		fmt.Println("JSON error")
 		return
 	}
-	assert.Equal(t, str, "{\"time\":\"2016-01-01 10:00:00\",\"host\":\"example.com\",\"type\":\"Fetch\",\"requestID\":\"928429348\",\"status\":200,\"elapsed\":238947,\"bytes\":2398,\"state\":\"Finished\"}")
+	assert.Equal(t, str, "{\"time\":\"2016-01-01 10:00:00\",\"host\":\"example.com\",\"type\":\"Fetch\",\"status\":200,\"elapsed-first-byte\":12345,\"elapsed-last-byte\":6789,\"elapsed\":4567,\"bytes\":4711,\"state\":\"Success\",\"instance\":\"aws-lambda-4711\"}")
 }
