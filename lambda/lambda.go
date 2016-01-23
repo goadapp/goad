faec5b41ef39d3404454cb25f84c4a909ab91ba7
Rasmus Sten <rasmus@Mac-Team-Demo-MacBook.local>
2016-01-23 15:28:40 +0200

    Simple client to put load on a web server

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -5,29 +5,66 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		fmt.Println("address was " + arg)
-		fetch(arg)
+	address := os.Args[1]
+	concurrencycount, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("ERROR %s\n", err)
+		return
 	}
+	requestcount, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Printf("ERROR %s\n", err)
+		return
+	}
+	fmt.Printf("Will spawn %d workers each making %d requests to %s\n", concurrencycount, requestcount, address)
+	runLoadTest(address, requestcount, concurrencycount)
 }
 
-func fetch(arg string) {
-	start := time.Now()
-	response, err := http.Get(arg)
-	if err != nil {
-		fmt.Printf("ERROR %s\n", err)
-		return
+func runLoadTest(url string, requestcount int, concurrencycount int) {
+	totalRequests := requestcount * concurrencycount
+	ch := make(chan string, totalRequests)
+	var wg sync.WaitGroup
+	for i := 0; i < concurrencycount; i++ {
+		wg.Add(1)
+		go fetch(url, requestcount, ch, &wg)
 	}
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Printf("ERROR %s\n", err)
-		return
+	fmt.Println("Waiting for results…")
+
+	completedRequests := 0
+	for completedRequests < totalRequests {
+		_ = <-ch
+		completedRequests++
+		fmt.Printf("\r%.2f%% done (%d requests out of %d)", (float64(completedRequests)/float64(totalRequests))*100.0, completedRequests, totalRequests)
 	}
-	fmt.Printf("%s\n", string(contents))
-	elapsed := time.Since(start)
-	fmt.Printf("%dnS\n", elapsed.Nanoseconds())
+	wg.Wait()
+	fmt.Printf("\nYay🎈\n")
+
+}
+
+func fetch(address string, requestcount int, ch chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Fetching %s %d times\n", address, requestcount)
+	for i := 0; i < requestcount; i++ {
+		start := time.Now()
+		response, err := http.Get(address)
+		if err != nil {
+			fmt.Printf("ERROR %s\n", err)
+			return
+		}
+		_, err = ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("ERROR %s\n", err)
+			return
+		}
+		//fmt.Printf("%s\n", string(contents))
+		elapsed := time.Since(start)
+		ch <- fmt.Sprintf("%d", elapsed.Nanoseconds())
+	}
+
 }
