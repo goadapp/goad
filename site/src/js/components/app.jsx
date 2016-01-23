2c0bcfd2dce363698690fa24644bd62b5887e878
Matias Korhonen <matias@kiskolabs.com>
2016-01-23 19:10:26 +0200

    Adds a demo results view

--- site/src/js/components/app.jsx
+++ site/src/js/components/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Form from './form.jsx';
+import Results from './results.jsx';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Form />
-    );
+    if (false) {
+      return (<Results url="https://example.invalid" />);
+    } else {
+      return (<Form />);
+    }
   }
 }
