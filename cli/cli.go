3b8959f5d3e2d334a15e1f03cb3446d2c3ec949d
Joao Carlos <mail@joao-carlos.com>
2016-01-24 11:59:20 +0200

    Start the CLI work

--- cli/cli.go
+++ cli/cli.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/gophergala2016/goad"
 )
 
+var (
+	url         string
+	concurrency uint
+	requests    uint
+	timeout     uint
+	region      string
+)
+
 func main() {
+	flag.UintVar(&concurrency, "c", 10, "number of concurrent requests")
+	flag.UintVar(&requests, "n", 1000, "number of total requests to make")
+	flag.UintVar(&timeout, "t", 15, "request timeout in seconds")
+	flag.StringVar(&region, "r", "us-east-1", "AWS region")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Println("You must specify a URL")
+		os.Exit(1)
+	}
+
+	url = flag.Args()[0]
+
 	test := goad.NewTest(&goad.TestConfig{
-		URL:            "http://dll.nu/",
-		Concurrency:    50,
-		TotalRequests:  1000,
-		RequestTimeout: time.Second,
-		Region:         "eu-west-1",
+		URL:            url,
+		Concurrency:    concurrency,
+		TotalRequests:  requests,
+		RequestTimeout: time.Duration(timeout) * time.Second,
+		Region:         region,
 	})
 	test.Start()
 }
