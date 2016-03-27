4627512e428d45173804298630d2a00309aec391
Joao Carlos <mail@joao-carlos.com>
2016-03-27 22:15:27 +0300

    Fix vendoring

--- cli/vendor/github.com/goadapp/goad/data/lambda/index.js
+++
@@ -1,9 +0,0 @@
-var execFile = require("child_process").execFile;
-
-exports.handler = function(event, context) {
-    child = execFile(event.file, event.args, function(error) {
-        context.done(error, "Process complete!");
-    });
-    child.stdout.on("data", console.log);
-    child.stderr.on("data", console.error);
-};
