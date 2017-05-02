886f8ea80d6d8ef07053e63671c304147a28da0e
Guido Serra <guido.serra@olx.com>
2017-05-02 11:38:46 +0200

    ...almost...

---
+++ HomebrewFormula/goad.rb
@@ -0,0 +1,24 @@
+class Goad < Formula
+  desc "An AWS Lambda powered, highly distributed, load testing tool built in Go."
+  homepage "https://goad.io/"
+  url "https://github.com/goadapp/goad.git"
+  version "1.4.0"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = buildpath/"bin"
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+
+    (buildpath/"src/github.com/goadapp/goad").install buildpath.children
+    cd "src/github.com/goadapp/goad/" do
+      system "make", "build"
+      bin.install "build/goad"
+    end
+  end
+
+  test do
+    system "#{bin}/goad", "--version"
+  end
+end
